688ad0888831b0c8f29fba52151058b3a4fd0e9a
vinimaffra03 <viniciushmdem15@gmail.com>
2025-03-12 16:31:16 -0300

    convertendo json

---
+++ converte-json.js
@@ -0,0 +1,11 @@
+const estudante = require('./estudante.json');
+
+const stringEstudante = JSON.stringify(estudante);
+
+console.log(stringEstudante);
+
+const objEstudante = JSON.parse(stringEstudante);
+
+console.log(objEstudante);
+console.log(objEstudante.telefones[1]);
+
